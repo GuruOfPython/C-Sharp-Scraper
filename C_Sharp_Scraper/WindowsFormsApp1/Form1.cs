d869c840f28f83e3a3fd2f4c92ad12d42f1674f9
GuruOfPython <vpeKJmPc@mail.com>
2019-01-03 04:03:33 -0800

    Initial Commit

---
+++ C_Sharp_Scraper/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Open_Browser()
+        {
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+            var driver = new ChromeDriver(driverService);
+            try
+            {
+                driver.Navigate().GoToUrl("https://money.cnn.com/data/hotstocks/index.html");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
